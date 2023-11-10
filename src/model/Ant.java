67b12708f084fba6b320aba49131e2aa47b6fdf5
Carlos Bermudez <carlos.bermudez01@usa.edu.co>
2023-11-10 09:47:10 -0500

    Diagrama base

--- src/model/Ant.java
+++ src/model/Ant.java
@@ -17,15 +17,10 @@ public class Ant {
         return posY;
     }
 
-   
-
-    // Otros métodos y atributos según sea necesario
-
     public void move(int newX, int newY, Environment environment) {
-        // Método para mover la hormiga a una nueva posición
-        environment.leavePheromone(this);  // Deja una feromona en la posición anterior
         // Método para mover la hormiga a una nueva posición
         this.posX = newX;
         this.posY = newY;
+        environment.leavePheromone(this);  // Deja una feromona en la nueva posición
     }
 }
